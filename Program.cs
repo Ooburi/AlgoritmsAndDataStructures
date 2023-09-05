59f7e13221c4d18a7e7801255ca511cbd0cf69b3
Pavel Andreevich <paandu.da@gmail.com>
2023-09-05 10:02:26 +0900

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoritmsAndDataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ S01E01/InsertionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsAndDataStructures.S01E01
+{
+    internal class InsertionSort
+    {
+        static void InsertionSortUp(int[] array)
+        {
+            for (int j = 1; j < array.Length; j++)
+            {
+                int key = array[j];
+                int i = j - 1;
+                while (i >= 0 && key < array[i])
+                {
+                    array[i + 1] = array[i];
+                    i--;
+                }
+                array[i + 1] = key;
+            }
+        }
+        static void InsertionSortDown(int[] array)
+        {
+            for (int j = 1; j < array.Length; j++)
+            {
+                int key = array[j];
+                int i = j - 1;
+                while (i >= 0 && key > array[i])
+                {
+                    array[i + 1] = array[i];
+                    i--;
+                }
+                array[i + 1] = key;
+            }
+        }
+    }
+
+}
